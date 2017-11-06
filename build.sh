3f5d3ce72987e6f4ae8f8caf92465a3fd9365d13
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-06 15:06:30 -0500

    try a different way to add

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 echo "Running the build.sh Script"
-export PERL_MM_USE_DEFAULT=1
-export PERL_EXTUTILS_AUTOINSTALL="--defaultdeps"
-perl -MCPAN -e 'install Math::BaseCalc'
+cpanm -i Math::BaseCalc
 mkdir -p $PREFIX/opt/
 cp $SRC_DIR/src/picard/* $PREFIX/opt/
