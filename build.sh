1e39e6586106a412b6349f4824e0d42b0b4fc72b
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-06 14:27:26 -0500

    put jars in opt

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ echo "Running the build.sh Script"
 export PERL_MM_USE_DEFAULT=1
 export PERL_EXTUTILS_AUTOINSTALL="--defaultdeps"
 perl -MCPAN -e 'install Math::BaseCalc'
+mkdir -p $PREFIX/opt/
+cp $SRC_DIR/src/picard/* $PREFIX/opt/
