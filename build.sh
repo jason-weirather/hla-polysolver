2c155aed519ea58aa9ea673b56de94b075b71657
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-07 09:47:21 -0500

    try to build that index at buildtime

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 echo "Running the build.sh Script"
 cpanm -i Math::BaseCalc
 mkdir -p $PREFIX/opt/
+mkdir -p $PREFIX/shared/
 cp $SRC_DIR/src/picard/* $PREFIX/opt/
+cp -r $SRC_DIR/src/data/ $PREFIX/shared/polysolver_data
+novoindex $PREFIX/shared/polysolver_data/abc_complete.nix $PREFIX/shared/polysolver_data/abc_complete.nix
