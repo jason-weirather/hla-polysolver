c5d1a68847fd5739ccb636b089eb0a0b2840bc0a
jason.weirather@gmail.com <jason-weirather@users.noreply.github.com>
2017-11-07 20:10:30 +0000

    moving towards a more installed version

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 echo "Running the build.sh Script"
 echo "Adding perl scripts"
 cpanm -i Math::BaseCalc
+mkdir -p $PREFIX/bin/
 mkdir -p $PREFIX/jar/
 mkdir -p $PREFIX/share/
+mkdir -p $PREFIX/scripts/
 echo "Adding picard jar files"
 cp $SRC_DIR/src/picard/* $PREFIX/jar/
 echo "Adding polysolver data"
 cp -r $SRC_DIR/data/ $PREFIX/share/polysolver_data
 echo "Rebuilding novoalign index"
 novoindex $PREFIX/share/polysolver_data/abc_complete.nix $PREFIX/share/polysolver_data/abc_complete.fasta
+echo "Put scripts into the polysolver build"
+cp $SRC_DIR/scripts/* $PREFIX/scripts/
--- config.bash
+++ config.bash
@@ -2,9 +2,9 @@
 
 # User modifiable variables
 
-export PSHOME=/home/hla-polysolver
+#export PSHOME=$CONDA_PREFIX/scripts
 export GATK_DIR=$CONDA_PREFIX/jar
-export MUTECT_DIR=$CONDA_PREFIX/jar
+export MUTECT_DIR=$CONDA_PREFIX/opt
 export STRELKA_DIR=$CONDA_PREFIX/bin
 export DATA_DIR=$CONDA_PREFIX/share/polysolver_data
 export TMP_DIR=/tmp
