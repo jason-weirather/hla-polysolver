adcd909cba60e1e6eeefef06fc48bdde8de7fcee
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-07 10:03:35 -0500

    update build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cpanm -i Math::BaseCalc
 mkdir -p $PREFIX/opt/
 mkdir -p $PREFIX/shared/
 cp $SRC_DIR/src/picard/* $PREFIX/opt/
-cp -r $SRC_DIR/src/data/ $PREFIX/shared/polysolver_data
-novoindex $PREFIX/shared/polysolver_data/abc_complete.nix $PREFIX/shared/polysolver_data/abc_complete.nix
+cp -r $SRC_DIR/data/ $PREFIX/shared/polysolver_data
+novoindex $PREFIX/shared/polysolver_data/abc_complete.nix $PREFIX/shared/polysolver_data/abc_complete.fasta
