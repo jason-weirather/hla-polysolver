d857d27664002701a8f8e3b4db545bf92643a74b
jason.weirather@gmail.com <jason-weirather@users.noreply.github.com>
2017-11-13 18:10:40 +0000

    fix incorrect pointer to strelka

--- config.bash
+++ config.bash
@@ -5,7 +5,7 @@
 #export PSHOME=$CONDA_PREFIX/scripts
 export GATK_DIR=$CONDA_PREFIX/jar
 export MUTECT_DIR=$CONDA_PREFIX/jar
-export STRELKA_DIR=$CONDA_PREFIX/include/strelka-upstream-v1.0.11
+export STRELKA_DIR=$CONDA_PREFIX/share/strelka/bin
 export DATA_DIR=$CONDA_PREFIX/share/polysolver_data
 export TMP_DIR=/tmp
 export NUM_THREADS=8
