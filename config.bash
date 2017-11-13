13307ea7906992d22d82fee18598d0c1be50b4a8
jason.weirather@gmail.com <jason-weirather@users.noreply.github.com>
2017-11-13 15:41:32 +0000

    fix mutect location

--- config.bash
+++ config.bash
@@ -4,7 +4,7 @@
 
 #export PSHOME=$CONDA_PREFIX/scripts
 export GATK_DIR=$CONDA_PREFIX/jar
-export MUTECT_DIR=$CONDA_PREFIX/opt
+export MUTECT_DIR=$CONDA_PREFIX/jar
 export STRELKA_DIR=$CONDA_PREFIX/include/strelka-upstream-v1.0.11
 export DATA_DIR=$CONDA_PREFIX/share/polysolver_data
 export TMP_DIR=/tmp
