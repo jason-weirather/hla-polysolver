9450a1bffc0c8e88a582a25c7b38aeed14172eaa
jason.weirather@gmail.com <jason-weirather@users.noreply.github.com>
2017-11-13 18:32:47 +0000

    fix path

--- config.bash
+++ config.bash
@@ -5,7 +5,7 @@
 #export PSHOME=$CONDA_PREFIX/scripts
 export GATK_DIR=$CONDA_PREFIX/jar
 export MUTECT_DIR=$CONDA_PREFIX/jar
-export STRELKA_DIR=$CONDA_PREFIX/share/strelka/bin
+export STRELKA_DIR=$CONDA_PREFIX/share/strelka
 export DATA_DIR=$CONDA_PREFIX/share/polysolver_data
 export TMP_DIR=/tmp
 export NUM_THREADS=8
